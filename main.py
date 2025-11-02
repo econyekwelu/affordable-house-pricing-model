eb93aa27f8481ec79549b682fb102c83e07a5728
Izagawd <theizaboss@gmail.com>
2025-11-02 16:31:02 -0400

    did renamings

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, mean_absolute_error, mean_squared_error
